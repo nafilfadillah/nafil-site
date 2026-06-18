f177139ac1b19c4e5a01000325477e59ecdbf7b6
nafilfadillah <nafilfadillah09@gmail.com>
2026-06-18 19:31:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -96,3 +96,19 @@ if(dot && outline){
     });
 
 }
+
+const themeToggle = document.getElementById("themeToggle");
+
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+
+    const icon = themeToggle.querySelector("i");
+
+    if(document.body.classList.contains("light-mode")){
+        icon.classList.remove("fa-moon");
+        icon.classList.add("fa-sun");
+    } else {
+        icon.classList.remove("fa-sun");
+        icon.classList.add("fa-moon");
+    }
+});
